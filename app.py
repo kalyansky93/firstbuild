13e318723411ce8ce08f4c9018f28a46c028b451
kalyansky93 <kalyanrockzzz99@gmail.com>
2024-12-29 09:48:36 +0530

    aftersuccessful run

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, World! Welcome to GCP and cloud build!"
+    return "Hello, World! Welcome to GCP and cloud build!,\n this is after successful run in gcp"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
